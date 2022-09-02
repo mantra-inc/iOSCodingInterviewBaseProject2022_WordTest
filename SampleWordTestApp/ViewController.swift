e8dbaa05acf63c18aba4ebeea23f98e867565dec
vinter <ankasandora@gmail.com>
2022-09-02 10:50:58 +0900

    Initial Commit

---
+++ SampleWordTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SampleWordTestApp
+//
+//  Created by 保田和彦 on 2022/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
